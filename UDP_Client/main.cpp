79821e03677d8e97306623ade65902aa13131f54
crh1781 <crh1781@g.rit.edu>
2018-11-19 01:08:20 -0500

    init commit

---
+++ UDP_Client/main.cpp
@@ -0,0 +1,117 @@
+#include <thread>
+#include <mutex>
+#include <stdio.h>
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+#define SERVER "192.168.1.13"	//ip address of udp server
+#define BUFFLEN 512	//Max length of buffer
+#define PORT 8888	//The port on which to listen for incoming data
+
+// Create client protocol
+struct sockaddr_in si_other;
+int sock, slen = sizeof(si_other);
+char buff[BUFFLEN];
+char message[BUFFLEN];
+WSADATA wsa;
+
+std::mutex m;
+bool is_listening = false;
+std::thread* listen_thread;
+
+void ListenForResponse()
+{
+	auto lock = std::unique_lock<std::mutex>(m);
+
+	//clear the buffer by filling null, it might have previously received data
+	memset(buff, '\0', BUFFLEN);
+	//try to receive some data, this is a blocking call
+	if (recvfrom(sock, buff, BUFFLEN, 0, (struct sockaddr *) &si_other, &slen) == SOCKET_ERROR)
+	{
+		printf("recvfrom() failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+
+	puts(buff);
+	is_listening = false;
+
+	lock.unlock();
+}
+
+int main(void)
+{
+	//Initialise winsock
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	printf("Initialised.\n");
+
+	//create socket
+	if ((sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == SOCKET_ERROR)
+	{
+		printf("socket() failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+
+	//setup address structure
+	memset((char *)&si_other, 0, sizeof(si_other));
+	si_other.sin_family = AF_INET;
+	si_other.sin_port = htons(PORT);
+	si_other.sin_addr.S_un.S_addr = inet_addr(SERVER);
+
+	// Setup user input thread
+	char io_buff[BUFFLEN];
+	bool quit = false;
+
+	auto io_thread = std::thread([&] {
+		while (!quit)
+		{
+			auto lock = std::unique_lock<std::mutex>(m);
+			printf("Enter message : ");
+			gets_s(io_buff);
+			if (strcmp(io_buff, "quit"))
+			{
+				quit == true;
+			}
+			lock.unlock();
+		}
+	});
+
+	//start communication
+	while (1)
+	{
+		//send the message
+		auto lock = std::unique_lock<std::mutex>(m);
+		if (sendto(sock, message, strlen(message), 0, (struct sockaddr *) &si_other, slen) == SOCKET_ERROR)
+		{
+			printf("sendto() failed with error code : %d", WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+		memset(io_buff, '\0', BUFFLEN); // Reset io buffer so we don't send the same message more than once
+		lock.unlock();
+
+		if (!is_listening)
+		{
+			lock.lock();
+			is_listening = true;
+			lock.unlock();
+			//receive a reply and print it
+			listen_thread = new std::thread(ListenForResponse);
+		}
+	}
+
+	io_thread.join();
+	listen_thread->join();
+	delete listen_thread;
+
+	closesocket(sock);
+	WSACleanup();
+
+	return 0;
+}
---
+++ UDP_Server/main.cpp
@@ -0,0 +1,152 @@
+#include <stdio.h>
+#include <thread>
+#include <mutex>
+
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#include <winsock2.h>
+
+#pragma comment(lib,"ws2_32.lib") //Winsock Library
+
+#define BUFFLEN 512	// Max length of buffer
+#define PORT 8888	// The port on which to listen for incoming data
+#define MAX_CLIENTS 16 // Max # of clients that can be connected at once
+
+SOCKET sock;
+struct sockaddr_in server, si_other;
+int slen, recv_len;
+char buff[BUFFLEN];
+char message[BUFFLEN];
+WSADATA wsa;
+
+int num_clients;
+bool is_client_connected[MAX_CLIENTS];
+std::thread client_threads[MAX_CLIENTS];
+struct sockaddr_in client_addresses[MAX_CLIENTS];
+
+int FindFreeClientIndex()
+{
+	for (int i = 0; i < MAX_CLIENTS; ++i)
+	{
+		if (!is_client_connected[i])
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+int FindExistingClientIndex(const struct sockaddr_in& address)
+{
+	for (int i = 0; i < MAX_CLIENTS; ++i)
+	{
+		if (is_client_connected[i] &&
+			strcmp(inet_ntoa(client_addresses[i].sin_addr), inet_ntoa(address.sin_addr)))
+		{
+			return i;
+		}
+	}
+	return -1;
+}
+
+bool IsClientConnected(int clientIndex)
+{
+	return is_client_connected[clientIndex];
+}
+
+const sockaddr_in& GetClientAddress(int clientIndex)
+{
+	return client_addresses[clientIndex];
+}
+
+void HandleClientSockets(const struct sockaddr_in& address, char* buff)
+{
+	//print details of the client/peer and the data received
+	printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+	printf("Data: %s\n", buff);
+
+	//now reply the client with the same data
+	if (sendto(sock, buff, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
+	{
+		printf("sendto() failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+}
+
+int main()
+{
+	slen = sizeof(si_other);
+
+	//Initialise winsock
+	printf("\nInitialising Winsock...");
+	if (WSAStartup(MAKEWORD(2, 2), &wsa) != 0)
+	{
+		printf("Failed. Error Code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	printf("Initialised.\n");
+
+	//Create a socket
+	if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) == INVALID_SOCKET)
+	{
+		printf("Could not create socket : %d", WSAGetLastError());
+	}
+	printf("Socket created.\n");
+
+	//Prepare the sockaddr_in structure
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_port = htons(PORT);
+
+	//Bind
+	if (bind(sock, (struct sockaddr *)&server, sizeof(server)) == SOCKET_ERROR)
+	{
+		printf("Bind failed with error code : %d", WSAGetLastError());
+		exit(EXIT_FAILURE);
+	}
+	puts("Bind done");
+
+	//keep listening for data
+	while (1)
+	{
+		printf("Waiting for data...");
+		fflush(stdout);
+
+		//clear the buffer by filling null, it might have previously received data
+		memset(buff, '\0', BUFFLEN);
+
+		//try to receive some data, this is a blocking call
+		if ((recv_len = recvfrom(sock, buff, BUFFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
+		{
+			printf("recvfrom() failed with error code : %d", WSAGetLastError());
+			exit(EXIT_FAILURE);
+		}
+
+		// Check if client is a new connection
+		int clientIndex = FindExistingClientIndex(si_other);
+		if (clientIndex == -1)
+		{
+			// Check if a free connection slot is available
+			clientIndex = FindFreeClientIndex();
+			if (clientIndex > -1)
+			{
+				client_addresses[clientIndex] = si_other;
+				client_threads[clientIndex] = std::thread(HandleClientSockets, client_addresses[clientIndex], buff);
+			}
+		}
+		else
+		{
+			client_threads[clientIndex].join();
+			client_threads[clientIndex] = std::thread(HandleClientSockets, client_addresses[clientIndex], buff);
+		}
+	}
+
+	for (int i = 0; i < MAX_CLIENTS; ++i)
+	{
+		client_threads[i].join();
+	}
+
+	closesocket(sock);
+	WSACleanup();
+
+	return 0;
+}
